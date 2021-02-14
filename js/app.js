52c139fd697faf8f0e446b03fb9659b9f3d5e50a
emansawalha <emansawalha10@gmail.com>
2021-02-14 03:12:31 +0200

    Finish and deploy your About Me

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 'use strict';
-let score =0 ; 
+let score = 0;
 //Welcome Massage
-let userN = prompt('Hello, What is Your Name ') ;
+let userN = prompt('Hello, What is Your Name ');
 console.log(userN);
 alert(`U R Welcome ${userN} to my Website :)`);
 alert(`${userN} Lets Play Gussing Game :D`);
@@ -9,77 +9,76 @@ document.write(`Hello ${userN} `);
 
 
 // This is the first Prompt for user
-let userinput1='yes' ;
+let userinput1 = 'yes';
 let userinput2 = 'y';
-let userName = prompt('Do You know Me  ?') ;
-if(userName.toLowerCase() === userinput1 || userName.toLowerCase() === userinput2 )
-{
-  alert('Hmmmmm U R My Friend :)') ;
-  console.log(`${userinput1}${userinput2}${userName}`);
-}else {
+let userName = prompt('Do You know Me  ?');
+if (userName.toLowerCase() === userinput1 || userName.toLowerCase() === userinput2) {
+  alert('Hmmmmm U R My Friend :)');
 
-  alert('Please Read more about Me :( ') ;
+} else {
+
+  alert('Please Read more about Me :( ');
 }
 
 
 
 //this is the second prompt for first name
-let userinput3='YES' ;
+let userinput3 = 'YES';
 let userinput4 = 'Y';
-let input ='eman';
+let input = 'eman';
 let fName = prompt('Is My First Name  Eman ?');
-if( fName.toUpperCase() ===userinput3 || fName.toUpperCase() === userinput4){
-  alert( 'My First Name is Eman ' );
-  console.log('EMAN');
-  score = score+1;
-}else {
-  alert('please Read About me ') ;
-  console.log('no');
+if (fName.toUpperCase() === userinput3 || fName.toUpperCase() === userinput4) {
+  alert('My First Name is Eman ');
+
+  score = score + 1;
+} else {
+  alert('please Read About me ');
+
 }
 
 
 // this is third prompt for where do i live
-let userinput5='yes' ;
+let userinput5 = 'yes';
 let userinput6 = 'y';
 let cityname = prompt('Do I live in Irbid  ?');
-if( cityname.toLowerCase() === userinput5 || cityname.toLowerCase() === userinput6){
-  alert( 'I Live in irbid  ' );
-  console.log('irbid');
-  score = score+1;
-}else {
-  alert('please Read About me ') ;
-  console.log('no');
+if (cityname.toLowerCase() === userinput5 || cityname.toLowerCase() === userinput6) {
+  alert('I Live in irbid  ');
+
+  score = score + 1;
+} else {
+  alert('please Read About me ');
+
 }
 
 
 // this is fourth prompt about my degree
-let userinput7='yes' ;
+let userinput7 = 'yes';
 let userinput8 = 'y';
 let degree = prompt('Is My Degree Is M.Sc  ?');
-if( degree.toLowerCase()=== userinput7 || degree.toLowerCase()=== userinput8){
-  alert( 'I Have M.Sc ' );
-  console.log('M.Sc') ;
-  score = score+1;
-}else {
-  alert('please Read About me ') ;
-  console.log('no');
+if (degree.toLowerCase() === userinput7 || degree.toLowerCase() === userinput8) {
+  alert('I Have M.Sc ');
+
+  score = score + 1;
+} else {
+  alert('please Read About me ');
+
 }
 
 
 //this is fifth prompt about LTUC
-let userinput9='yes' ;
+let userinput9 = 'yes';
 let userinput10 = 'y';
 let LTUc = prompt(' do i attend to LTUC -ASAC ?')
-if( degree.toLowerCase()=== userinput9 || degree.toLowerCase() === userinput10){
-  alert( 'iam LTUC - ASAC Student  ' );
-  console.log('LTUC') ;
-  score = score+1;
-}else {
-  alert('please Read About me ') ;
-  console.log('no')
+if (degree.toLowerCase() === userinput9 || degree.toLowerCase() === userinput10) {
+  alert('iam LTUC - ASAC Student  ');
+
+  score = score + 1;
+} else {
+  alert('please Read About me ');
+
 }
 
-let aboutMe= alert(`U R Most Welcom ${userN},My Name is Eman, i live in irbid, i have M.Sc degree and i am student @ LTUC _ ASAC `) ;
+let aboutMe = alert(`U R Most Welcom ${userN},My Name is Eman, i live in irbid, i have M.Sc degree and i am student @ LTUC _ ASAC `);
 
 //class - 03
 /*Add a 6th question to the guessing game that takes in a numeric input by prompting the user to guess a number.
@@ -87,33 +86,33 @@ Indicates through an alert if the guess is “too high” or “too low”.
 It should give the user exactly four opportunities to get the correct answer.
 After all attempts have been exhausted, tell the user the correct answer. Consider using a loop of some sort.*/
 
-// Question No.1:what is my age ?
+// Question No.1:what is my age ?*/
 
-let correct= 34;
-let i=0 ;
-for(i =1 ; i<=4 ; i=i+1){
+let correct = 34;
+let i = 0;
+for (i = 1; i <= 4; i = i + 1) {
   let question1 = prompt('what is my age ?');
   question1 = parseInt(question1);
 
-  if (question1 === correct){
-    alert('I have '+correct +':)');
-    console.log(question1);
-    score = score+1;
+  if (question1 === correct) {
+    alert('I have ' + correct + ':)');
+
+    score = score + 1;
     break;
   }
   else {
-    if(question1>34)
-    {
+    if (question1 > 34) {
       alert('too high');
       alert('Try again');
-      console.log('too high');
 
-    }else if(question1<34){
+
+    } else if (question1 < 34) {
       alert('too low');
-      console.log('too low');
-      alert('Try again');}
-  } 
-  if(i===4){
+
+      alert('Try again');
+    }
+  }
+  if (i === 4) {
     alert('You finish your attampts');
     alert('i have 34 years ');
   }
@@ -131,28 +130,27 @@ Consider using a loop of some sort for this question.
 //Question No.7*/
 
 
-let Arrcourse = ['html','js','css','java','python'];
+let Arrcourse = ['html', 'js', 'css', 'java', 'python'];
 
 
 let found = false;
-for( i = 0 ; i<=6 ; i++){
+for (i = 0; i <= 6; i++) {
   let userAns = prompt('what is the most PL used to build webpages?');
-  for (let j=0 ; j<= Arrcourse.length; j++){
-    if( userAns.toLowerCase()=== Arrcourse[j])
-    {
-      alert ('yes');
-      console.log(userAns);
-      score = score+1;
+  for (let j = 0; j <= Arrcourse.length; j++) {
+    if (userAns.toLowerCase() === Arrcourse[j]) {
+      alert('yes');
+
+      score = score + 1;
       found = true;
       break;
     }
   }
-if(found === true){
-console.log('n');
- break;
- }
+  if (found === true) {
+
+    break;
+  }
 }
-alert('these are some PL using to in webdevelopment: '+ Arrcourse);
-alert('Your score is '+ '  '+ score + 'out of 7');
+alert('these are some PL using to in webdevelopment: ' + Arrcourse);
+alert('Your score is ' + '  ' + score + 'out of 7');
 alert(`Thanks ${userN} for your information :)`)
 
